44ce6169b5e2a9f34a5a2998eced8ca090813567
horsaen <atealltheglue@protonmail.com>
2022-07-03 00:25:41 -0500

    working db connection

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
+mongoose.connect(process.env.DATABASE_URL);
+mongoose.connection.on("connected", () => {
+    console.log("DB: Connected")
+});
+
 app.listen(PORT, () => {
     console.log('API is now listening on port ' + PORT)
-})
+});
