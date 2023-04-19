a2f087b39119106a1d041d1eaec996abad958d0d
horsaen <atealltheglue@protonmail.com>
2023-04-18 22:02:56 -0500

    models complete

--- models/recreation/recreation.js
+++ models/recreation/recreation.js
@@ -1,7 +1,32 @@
 const mongoose = require('mongoose');
 
 const recreationSchema = new mongoose.Schema({
-
+    coverImg: {
+        type: String
+    },
+    mainImg: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    mapsAddress: {
+        type: String
+    },
+    history: {
+        type: String
+    },
+    activities: {
+        type: String
+    },
+    socials: [{
+        type: {
+            type: String
+        },
+        user: {
+            type: String
+        }
+    }]
 })
 
 module.exports = mongoose.model('recreation', recreationSchema)
--- models/townships/townships.js
+++ models/townships/townships.js
@@ -1,7 +1,29 @@
 const mongoose = require('mongoose');
 
 const townshipsSchema = new mongoose.Schema({
-
+    coverImg: {
+        type: String
+    },
+    mainImg: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    url: {
+        type: String
+    },
+    mapsAddress: {
+        type: String
+    },
+    socials: [{
+        type: {
+            type: String
+        },
+        user: {
+            type: String
+        }
+    }]
 })
 
 module.exports = mongoose.model('townships', townshipsSchema)
